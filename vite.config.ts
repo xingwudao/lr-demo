1b1c769c6206c999909d3ff6c3c06a23a8aa96c2
xingwudao <kaijiangchen@gmail.com>
2025-04-05 23:55:07 +0800

    Add Vercel deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    sourcemap: false,
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    }
+  }
 }) 
